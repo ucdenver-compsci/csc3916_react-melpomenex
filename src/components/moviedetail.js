87cb9241007c6ddab79a1f68bef355b9216f26bd
Ben Lovell <gnomer@gmail.com>
2024-04-14 13:08:07 -0600

    Third Commit

--- src/components/moviedetail.js
+++ src/components/moviedetail.js
@@ -1,61 +1,62 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { fetchMovie } from "../actions/movieActions";
-import {connect} from 'react-redux';
-import {Card, ListGroup, ListGroupItem } from 'react-bootstrap';
-import { BsStarFill } from 'react-icons/bs'
+import { Card, ListGroup, ListGroupItem } from 'react-bootstrap';
+import { BsStarFill } from 'react-icons/bs';
 import { Image } from 'react-bootstrap';
 import ReviewForm from './reviewform';
 
 class MovieDetail extends Component {
-
     componentDidMount() {
-        const {dispatch} = this.props;
-        if (this.props.selectedMovie == null) {
-            dispatch(fetchMovie(this.props.movieId));
+        const { dispatch, selectedMovie, movieId } = this.props;
+        if (!selectedMovie) {
+            dispatch(fetchMovie(movieId));
         }
     }
 
     render() {
-        const DetailInfo = () => {
-            if (!this.props.selectedMovie) {
-                return <div>Loading....</div>
-            }
+        const { selectedMovie } = this.props;
 
-            return (
-                <Card>
-                    <Card.Header>Movie Detail</Card.Header>
-                    <Card.Body>
-                        <Image className="image" src={this.props.selectedMovie.imageUrl} thumbnail />
-                    </Card.Body>
-                    <ListGroup>
-                        <ListGroupItem>{this.props.selectedMovie.title}</ListGroupItem>
-                        <ListGroupItem>
-                            {this.props.selectedMovie.actors.map((actor, i) =>
-                                <p key={i}>
-                                    <b>{actor.actorName}</b> {actor.characterName}
-                                </p>)}
-                        </ListGroupItem>
-                        <ListGroupItem><h4><BsStarFill/> {this.props.selectedMovie.avgRating}</h4></ListGroupItem>
-                    </ListGroup>
-                    <Card.Body>
-                        {this.props.selectedMovie.reviews.map((review, i) =>
-                            <p key={i}>
-                                <b>{review.username}</b>&nbsp; {review.review}
-                                &nbsp;  <BsStarFill /> {review.rating}
-                            </p>
-                        )}
-                    </Card.Body>
-                    <Card.Body>
-                        <h5>Leave a review</h5>
-                        <ReviewForm movieId = {movieId}/>
-                    </Card.Body>
-                </Card>
-            )
+        if (!selectedMovie) {
+            return <div>Loading....</div>;
         }
 
         return (
-            <DetailInfo />
-        )
+            <Card>
+                <Card.Header>Movie Detail</Card.Header>
+                <Card.Body>
+                    {selectedMovie.imageUrl && (
+                        <Image className="image" src={selectedMovie.imageUrl} thumbnail />
+                    )}
+                </Card.Body>
+                <ListGroup>
+                    <ListGroupItem>{selectedMovie.title}</ListGroupItem>
+                    <ListGroupItem>
+                        {selectedMovie.actors && selectedMovie.actors.map((actor, i) => (
+                            <p key={i}>
+                                <b>{actor.actorName}</b> {actor.characterName}
+                            </p>
+                        ))}
+                    </ListGroupItem>
+                    <ListGroupItem>
+                        <h4><BsStarFill/> {selectedMovie.avgRating}</h4>
+                    </ListGroupItem>
+                </ListGroup>
+                <Card.Body>
+                    {selectedMovie.reviews && selectedMovie.reviews.map((review, i) => (
+                        <div key={i}>
+                            <p><b>{review.username}</b>&nbsp; {review.review}</p>
+                            <p><BsStarFill /> {review.rating}</p>
+                        </div>
+                    ))}
+                </Card.Body>
+                    {/* Render the ReviewForm component */}
+                <Card.Body>
+                    <h5>Leave a Review</h5>
+                    <ReviewForm movieId={movieId} />
+                </Card.Body>
+            </Card>
+        );
     }
 }
 
