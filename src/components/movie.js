2d996befbb083d6c0ecc037a4b496696b17c93f9
Ben Lovell <gnomer@gmail.com>
2024-04-14 14:39:40 -0600

    Update moviedetail

--- src/components/movie.js
+++ src/components/movie.js
@@ -1,22 +1,69 @@
-import React, { useState } from 'react';
-import { useParams } from 'react-router-dom';
-import { useDispatch } from 'react-redux'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { fetchMovie } from "../actions/movieActions";
-import MovieDetail from "../components/moviedetail"
+import { Card, ListGroup, ListGroupItem } from 'react-bootstrap';
+import { BsStarFill } from 'react-icons/bs';
+import { Image } from 'react-bootstrap';
+import ReviewForm from './reviewform';
 
-// support routing
-
-function Movie(props) {
-    const [selectedMovie] = useState(props.selectedMovie);
-    const params = useParams();
-    const movieId = params.movieId;
-    console.log(movieId);
-    const dispatch = useDispatch();
-    if (selectedMovie == null) {
-        dispatch(fetchMovie(movieId));
+class MovieDetail extends Component {
+    componentDidMount() {
+        const { dispatch, selectedMovie, movieId } = this.props;
+        if (!selectedMovie) {
+            dispatch(fetchMovie(movieId));
+        }
     }
 
-    return (<MovieDetail movieId={movieId} />)
+    render() {
+        const { selectedMovie } = this.props;
+
+        if (!selectedMovie) {
+            return <div>Loading....</div>;
+        }
+
+        return (
+            <Card>
+                <Card.Header>Movie Detail</Card.Header>
+                <Card.Body>
+                    {selectedMovie.imageUrl && (
+                        <Image className="image" src={selectedMovie.imageUrl} thumbnail />
+                    )}
+                </Card.Body>
+                <ListGroup>
+                    <ListGroupItem>{selectedMovie.title}</ListGroupItem>
+                    <ListGroupItem>
+                        {selectedMovie.actors && selectedMovie.actors.map((actor, i) => (
+                            <p key={i}>
+                                <b>{actor.actorName}</b> {actor.characterName}
+                            </p>
+                        ))}
+                    </ListGroupItem>
+                    <ListGroupItem>
+                        <h4><BsStarFill/> {selectedMovie.avgRating}</h4>
+                    </ListGroupItem>
+                </ListGroup>
+                <Card.Body>
+                    {selectedMovie.reviews && selectedMovie.reviews.map((review, i) => (
+                        <div key={i}>
+                            <p><b>{review.username}</b>&nbsp; {review.review}</p>
+                            <p><BsStarFill /> {review.rating}</p>
+                        </div>
+                    ))}
+                </Card.Body>
+                    {/* Render the ReviewForm component */}
+                <Card.Body>
+                    <h5>Leave a Review</h5>
+                    <ReviewForm movieId={movieId} />
+                </Card.Body>
+            </Card>
+        );
+    }
 }
 
-export default Movie;
+const mapStateToProps = state => {
+    return {
+        selectedMovie: state.movie.selectedMovie
+    }
+}
+
+export default connect(mapStateToProps)(MovieDetail);
--- src/components/moviedetail.js
+++ src/components/moviedetail.js
@@ -1,62 +1,56 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
 import { fetchMovie } from "../actions/movieActions";
-import { Card, ListGroup, ListGroupItem } from 'react-bootstrap';
-import { BsStarFill } from 'react-icons/bs';
+import {connect} from 'react-redux';
+import {Card, ListGroup, ListGroupItem } from 'react-bootstrap';
+import { BsStarFill } from 'react-icons/bs'
 import { Image } from 'react-bootstrap';
-import ReviewForm from './reviewform';
 
 class MovieDetail extends Component {
+
     componentDidMount() {
-        const { dispatch, selectedMovie, movieId } = this.props;
-        if (!selectedMovie) {
-            dispatch(fetchMovie(movieId));
+        const {dispatch} = this.props;
+        if (this.props.selectedMovie == null) {
+            dispatch(fetchMovie(this.props.movieId));
         }
     }
 
     render() {
-        const { selectedMovie } = this.props;
+        const DetailInfo = () => {
+            if (!this.props.selectedMovie) {
+                return <div>Loading....</div>
+            }
 
-        if (!selectedMovie) {
-            return <div>Loading....</div>;
+            return (
+                <Card>
+                    <Card.Header>Movie Detail</Card.Header>
+                    <Card.Body>
+                        <Image className="image" src={this.props.selectedMovie.imageUrl} thumbnail />
+                    </Card.Body>
+                    <ListGroup>
+                        <ListGroupItem>{this.props.selectedMovie.title}</ListGroupItem>
+                        <ListGroupItem>
+                            {this.props.selectedMovie.actors.map((actor, i) =>
+                                <p key={i}>
+                                    <b>{actor.actorName}</b> {actor.characterName}
+                                </p>)}
+                        </ListGroupItem>
+                        <ListGroupItem><h4><BsStarFill/> {this.props.selectedMovie.avgRating}</h4></ListGroupItem>
+                    </ListGroup>
+                    <Card.Body>
+                        {this.props.selectedMovie.reviews.map((review, i) =>
+                            <p key={i}>
+                                <b>{review.username}</b>&nbsp; {review.review}
+                                &nbsp;  <BsStarFill /> {review.rating}
+                            </p>
+                        )}
+                    </Card.Body>
+                </Card>
+            )
         }
 
         return (
-            <Card>
-                <Card.Header>Movie Detail</Card.Header>
-                <Card.Body>
-                    {selectedMovie.imageUrl && (
-                        <Image className="image" src={selectedMovie.imageUrl} thumbnail />
-                    )}
-                </Card.Body>
-                <ListGroup>
-                    <ListGroupItem>{selectedMovie.title}</ListGroupItem>
-                    <ListGroupItem>
-                        {selectedMovie.actors && selectedMovie.actors.map((actor, i) => (
-                            <p key={i}>
-                                <b>{actor.actorName}</b> {actor.characterName}
-                            </p>
-                        ))}
-                    </ListGroupItem>
-                    <ListGroupItem>
-                        <h4><BsStarFill/> {selectedMovie.avgRating}</h4>
-                    </ListGroupItem>
-                </ListGroup>
-                <Card.Body>
-                    {selectedMovie.reviews && selectedMovie.reviews.map((review, i) => (
-                        <div key={i}>
-                            <p><b>{review.username}</b>&nbsp; {review.review}</p>
-                            <p><BsStarFill /> {review.rating}</p>
-                        </div>
-                    ))}
-                </Card.Body>
-                    {/* Render the ReviewForm component */}
-                <Card.Body>
-                    <h5>Leave a Review</h5>
-                    <ReviewForm movieId={movieId} />
-                </Card.Body>
-            </Card>
-        );
+            <DetailInfo />
+        )
     }
 }
 
@@ -67,4 +61,3 @@ const mapStateToProps = state => {
 }
 
 export default connect(mapStateToProps)(MovieDetail);
-
