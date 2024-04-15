e89fed33872fe61b8741bd8a1397d42e9dfe6c2e
Ben Lovell <gnomer@gmail.com>
2024-04-14 20:26:46 -0600

    .

--- src/components/movie.js
+++ src/components/movie.js
@@ -10,74 +10,25 @@ import { BsFillStarFill } from 'react-icons/bs';
 
 // support routing by creating a new component
 
-class Movie extends Component {
-    componentDidMount() {
-        const {dispatch} = this.props;
-        if (this.props.selectedMovie == null) {
-            dispatch(fetchMovie(this.props.movieId));
-        }
+import React, { useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { useDispatch } from 'react-redux'
+import { fetchMovie } from "../actions/movieActions";
+import MovieDetail from "../components/moviedetail"
+
+// support routing
+
+function Movie(props) {
+    const [selectedMovie] = useState(props.selectedMovie);
+    const params = useParams();
+    const title = params.title;
+    console.log(title);
+    const dispatch = useDispatch();
+    if (selectedMovie == null) {
+        dispatch(fetchMovie(title));
     }
 
-    render() {
-        const ActorInfo = ({actors}) => {
-            return actors.map((actor, i) =>
-                <p key={i}>
-                    <b>{actor.actorName}</b> {actor.characterName}
-                </p>
-            );
-        }
-
-        const ReviewInfo = ({reviews}) => {
-            return reviews.map((review, i) =>
-                <p key={i}>
-                    <b>{review.username}</b>&nbsp; {review.review}
-                    &nbsp;  <BsStarFill /> {review.rating}
-                </p>
-            );
-        }
-
-        const DetailInfo = ({currentMovie}) => {
-            if (!currentMovie) {
-                return <div>Loading....</div>
-            }
-            return (
-                <Panel>
-                    <Panel.Heading>Movie Detail</Panel.Heading>
-                    <Panel.body><Image className="image" src={currentMovie.imageUrl} thumbnail /></Panel.body>
-                    <ListGroup>
-                        <ListGroupItem>{currentMovie.title}</ListGroupItem>
-                        <ListGroupItem>
-                            <ActorInfo actors={currentMovie.actors} />
-                        </ListGroupItem>
-                        <ListGroupItem><h4><Glyphicon/></h4> </ListGroupItem>
-                    </ListGroup>
-                </Panel>
-            )
-        }
-
-        const mapStateToProps = (state, ownProps) => {
-            console.log(ownProps);
-            return {
-                selectedMovie: state.movie.selectedMovie,
-                movieId: ownProps.match.params.movieId
-            }
-        }
-    }
+    return (<MovieDetail title={title} />)
 }
 
 export default Movie;
-
-//function Movie(props) {
-//    const [selectedMovie] = useState(props.selectedMovie);
-//    const params = useParams();
-//    const movieId = params.movieId;
-//    console.log(movieId);
-//    const dispatch = useDispatch();
-//    if (selectedMovie == null) {
-//        dispatch(fetchMovie(movieId));
-//    }
-//
-//    return (<MovieDetail movieId={movieId} />)
-//}
-
-// export default Movie;
