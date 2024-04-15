83d218a83f70c84f3079ada5823fa10e7eb63bba
Ben Lovell <gnomer@gmail.com>
2024-04-14 20:24:05 -0600

    update movie.js

--- src/components/movie.js
+++ src/components/movie.js
@@ -4,7 +4,7 @@ import { useDispatch } from 'react-redux'
 import { fetchMovie } from "../actions/movieActions";
 import MovieDetail from "../components/moviedetail"
 import { bindActionCreators } from 'redux';
-import { Panel, Image, ListGroup, ListGroupItem } from 'react-bootstrap';
+import { Glyphicon, Panel, Image, ListGroup, ListGroupItem } from 'react-bootstrap';
 import { BsStarFill } from 'react-icons/bs';
 import { BsFillStarFill } from 'react-icons/bs';
 
@@ -49,7 +49,7 @@ class Movie extends Component {
                         <ListGroupItem>
                             <ActorInfo actors={currentMovie.actors} />
                         </ListGroupItem>
-                        <ListGroupItem><h4><BsFillStarFill/></h4> </ListGroupItem>
+                        <ListGroupItem><h4><Glyphicon/></h4> </ListGroupItem>
                     </ListGroup>
                 </Panel>
             )
